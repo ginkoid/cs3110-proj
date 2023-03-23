be8fe1581af96530d2ea65dc288bd61328846cf8
Philip <g@gnk.io>
2023-03-22 20:03:10 -0400

    feat: add interactivity

--- src/main.ml
+++ src/main.ml
@@ -12,32 +12,113 @@ type board = cell list list
 let js = Js.string
 let doc = Html.document
 
-let dom_of_board b =
-  let div className =
-    let div = Html.createDiv doc in
-    div##.className := js className;
-    div
-  in
-  let els =
-    List.map
-      (function
-        | Empty -> div "empty"
-        | Shined -> div "shined"
-        | Filled n ->
-            let el = div "filled" in
-            el##.innerText := js (string_of_int n);
-            el
-        | Light -> div "light")
-      (List.flatten b)
-  in
-  let grid = div "grid" in
-  List.iter (Dom.appendChild grid) els;
-  grid
-
 let demo_board =
   [
-    [ Shined; Shined; Light; Filled 1 ];
-    [ Filled 3; Empty; Shined; Shined ];
-    [ Empty; Empty; Filled 2; Light ];
-    [ Empty; Empty; Empty; Shined ];
+    [ Empty; Empty; Empty; Empty; Empty; Filled 1; Empty; Empty; Empty; Empty ];
+    [ Empty; Empty; Empty; Empty; Empty; Empty; Empty; Empty; Empty; Empty ];
+    [
+      Empty; Empty; Filled 4; Empty; Empty; Empty; Filled 1; Empty; Empty; Empty;
+    ];
+    [ Empty; Empty; Empty; Empty; Empty; Empty; Empty; Empty; Empty; Empty ];
+    [ Empty; Empty; Empty; Empty; Empty; Empty; Empty; Empty; Empty; Empty ];
+    [
+      Filled 1; Empty; Empty; Empty; Empty; Empty; Empty; Filled 3; Empty; Empty;
+    ];
+    [ Empty; Empty; Empty; Empty; Empty; Empty; Empty; Empty; Empty; Empty ];
+    [
+      Empty; Empty; Empty; Empty; Filled 2; Empty; Empty; Empty; Filled 1; Empty;
+    ];
+    [ Empty; Empty; Empty; Empty; Empty; Empty; Filled 1; Empty; Empty; Empty ];
+    [ Empty; Empty; Empty; Empty; Empty; Empty; Empty; Empty; Empty; Empty ];
   ]
+
+let render_shined board =
+  List.mapi
+    (fun y ->
+      List.mapi (fun x -> function
+        | Empty ->
+            let rec check x y (dx, dy) =
+              if
+                x < 0 || y < 0
+                || x >= List.length board
+                || y >= List.length board
+              then false
+              else
+                match List.nth (List.nth board y) x with
+                | Empty -> check (x + dx) (y + dy) (dx, dy)
+                | Light -> true
+                | _ -> false
+            in
+            let checkdir = check x y in
+            if
+              checkdir (1, 0)
+              || checkdir (-1, 0)
+              || checkdir (0, 1)
+              || checkdir (0, -1)
+            then Shined
+            else Empty
+        | a -> a))
+    board
+
+let div className =
+  let div = Html.createDiv doc in
+  div##.className := js className;
+  div
+
+let dom_of_board cb board =
+  let els =
+    List.mapi
+      (fun y ->
+        List.mapi (fun x cell ->
+            let el =
+              match cell with
+              | Empty -> div "empty"
+              | Shined -> div "shined"
+              | Filled n ->
+                  let el = div "filled" in
+                  el##.innerText := js (string_of_int n);
+                  el
+              | Light -> div "light"
+            in
+            el##.onclick :=
+              Html.handler (fun _ ->
+                  cb board x y;
+                  Js._true);
+            el))
+      (render_shined board)
+  in
+  let grid = div "grid" in
+  let _ =
+    Js.Unsafe.meth_call grid##.style "setProperty"
+      [|
+        Js.Unsafe.inject (js "--size");
+        Js.Unsafe.inject (js (string_of_int (List.length board)));
+      |]
+  in
+  List.iter (Dom.appendChild grid) (List.flatten els);
+  grid
+
+let click_board board x y =
+  List.mapi
+    (fun y' ->
+      List.mapi (fun x' cell ->
+          if x' = x && y' = y then
+            match cell with
+            | Empty -> Light
+            | Light -> Empty
+            | a -> a
+          else cell))
+    board
+
+let game board =
+  let root = div "game" in
+  let rec update_board board x y =
+    let board' = click_board board x y in
+    let _ =
+      Js.Unsafe.meth_call root "replaceChildren"
+        [| Js.Unsafe.inject (dom_of_board update_board board') |]
+    in
+    ()
+  in
+  let _ = Dom.appendChild root (dom_of_board update_board board) in
+  root
--- src/main.mli
+++ src/main.mli
@@ -1,4 +1,4 @@
 type board
 
-val dom_of_board : board -> Js_of_ocaml.Dom_html.element Js_of_ocaml.Js.t
+val game : board -> Js_of_ocaml.Dom_html.element Js_of_ocaml.Js.t
 val demo_board : board
