2624812b89ed5278bc99e701e5448008f33cca0d
Haseeb Saleem Atcha <hsa34@cornell.edu>
2023-03-26 15:39:40 -0400

    Update create_game.ml

---
+++ src/create_game.ml
@@ -0,0 +1,18 @@
+type difficulty = 
+  |Easy
+  |Normal
+  |Hard
+
+
+let gen_white_sq
+
+let gen_black_sq
+
+let gen_num_blocks board =
+
+
+let gen_board board difficulty  = function
+  |Easy -> board |> gen_num_blocks |> 
+  |Normal -> board |> gen_num_blocks |>
+  |Hard -> board |>  gen_num_blocks |>
+  
